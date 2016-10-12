f7008e4c32a0f60db9e29a4bac117db091149fbc
zacapa <mutomasahiro1111@gmail.com>
2016-10-12 15:03:45 +0900

    modify

--- nico_lstm.py
+++ nico_lstm.py
@@ -13,7 +13,7 @@ from chainer import training
 from chainer.training import extensions
 
 
-# Definition of a recurrent net for language modeling
+
 class RNNForLM(chainer.Chain):
 
     def __init__(self, n_vocab, n_units, train=True):
@@ -39,40 +39,22 @@ class RNNForLM(chainer.Chain):
         return y
 
 
-# Dataset iterator to create a batch of sequences at different positions.
-# This iterator returns a pair of current words and the next words. Each
-# example is a part of sequences starting from the different offsets
-# equally spaced within the whole sequence.
+
 class ParallelSequentialIterator(chainer.dataset.Iterator):
 
     def __init__(self, dataset, batch_size, repeat=True):
         self.dataset = dataset
-        self.batch_size = batch_size  # batch size
-        # Number of completed sweeps over the dataset. In this case, it is
-        # incremented if every word is visited at least once after the last
-        # increment.
+        self.batch_size = batch_size
         self.epoch = 0
-        # True if the epoch is incremented at the last iteration.
         self.is_new_epoch = False
         self.repeat = repeat
         length = len(dataset)
-        # Offsets maintain the position of each sequence in the mini-batch.
         self.offsets = [i * length // batch_size for i in range(batch_size)]
-        # NOTE: this is not a count of parameter updates. It is just a count of
-        # calls of ``__next__``.
         self.iteration = 0
 
     def __next__(self):
-        # This iterator returns a list representing a mini-batch. Each item
-        # indicates a different position in the original sequence. Each item is
-        # represented by a pair of two word IDs. The first word is at the
-        # "current" position, while the second word at the next position.
-        # At each iteration, the iteration count is incremented, which pushes
-        # forward the "current" position.
         length = len(self.dataset)
         if not self.repeat and self.iteration * self.batch_size >= length:
-            # If not self.repeat, this iterator stops at the end of the first
-            # epoch (i.e., when all words are visited once).
             raise StopIteration
         cur_words = self.get_words()
         self.iteration += 1
@@ -87,21 +69,17 @@ class ParallelSequentialIterator(chainer.dataset.Iterator):
 
     @property
     def epoch_detail(self):
-        # Floating point version of epoch.
         return self.iteration * self.batch_size / len(self.dataset)
 
     def get_words(self):
-        # It returns a list of current words.
         return [self.dataset[(offset + self.iteration) % len(self.dataset)]
                 for offset in self.offsets]
 
     def serialize(self, serializer):
-        # It is important to serialize the state to be recovered on resume.
         self.iteration = serializer('iteration', self.iteration)
         self.epoch = serializer('epoch', self.epoch)
 
 
-# Custom updater for truncated BackProp Through Time (BPTT)
 class BPTTUpdater(training.StandardUpdater):
 
     def __init__(self, train_iter, optimizer, bprop_len, device):
@@ -109,29 +87,19 @@ class BPTTUpdater(training.StandardUpdater):
             train_iter, optimizer, device=device)
         self.bprop_len = bprop_len
 
-    # The core part of the update routine can be customized by overriding.
     def update_core(self):
         loss = 0
-        # When we pass one iterator and optimizer to StandardUpdater.__init__,
-        # they are automatically named 'main'.
         train_iter = self.get_iterator('main')
         optimizer = self.get_optimizer('main')
 
         # Progress the dataset iterator for bprop_len words at each iteration.
         for i in range(self.bprop_len):
-            # Get the next batch (a list of tuples of two word IDs)
             batch = train_iter.__next__()
-
-            # Concatenate the word IDs to matrices and send them to the device
-            # self.converter does this job
-            # (it is chainer.dataset.concat_examples by default)
-            # x, t = self.converter(batch, self.device)
             x = np.asarray([example[0] for example in batch], dtype=np.int32)
             t = np.asarray([example[1] for example in batch], dtype=np.int32)
             if self.device >= 0:
                 x = chainer.cuda.to_gpu(x, device=self.device)
                 t = chainer.cuda.to_gpu(t, device=self.device)
-            # Compute the loss at this time step and accumulate it
             loss += optimizer.target(chainer.Variable(x), chainer.Variable(t))
 
         optimizer.target.cleargrads()  # Clear the parameter gradients
